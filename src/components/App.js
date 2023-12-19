b62cee26182ce055713492cbbc3c07fdcca88204
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-19 11:56:20 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const App = () => {
+
+  const relatives = ['Hanuman Ji', 'Ram Ji', 'Sita Maa', 'Lakshaman Bhaiya', 'Brahma Ji', 'Vishnu Ji', 'Shankar Ji' ]
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ul>
+                {relatives.map((item)=>{
+
+                  return  <li key={item}>{item}</li>
+                })}
+               </ul>
     </div>
   )
 }
